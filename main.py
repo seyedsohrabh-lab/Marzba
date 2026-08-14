57b8f2799fc2c3b7bbe1319ad2a61b54dc3613c4
seyedsohrabh-lab <seyedsohrabh@gmail.com>
2026-08-14 14:05:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ Use the following command:
 Then, navigate to {click.style(f'http://127.0.0.1:{UVICORN_PORT}', bold=True)} on your computer.
             """)
 
-            bind_args['host'] = '127.0.0.1'
+bind_args['host'] = UVICORN_HOST
             bind_args['port'] = UVICORN_PORT
 
     if DEBUG:
